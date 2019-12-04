92f68585d52e20b30e6bbf9f6103f93a05052e2e
Roger Dodger <lpaddikt@gmail.com>
2019-12-03 23:59:01 -0800

    cart format change

--- public/js/Cart.js
+++ public/js/Cart.js
@@ -1,49 +1,39 @@
-'use strict'
+'use strict';
 
-class Cart{
-
-    static _addToCart(product = null, cart){
-        let index = this._inCart(product, cart);
-        if(index != -1){
-            cart.items[index].qty += 1;
-        } else {
-        let item = {
-            id: product.id,
-            imageURL: product.imageURL,
-            description: product.description,
-            price: product.price,
-            qty: product.qty,
-        }
-        cart.items.push(item);
-        }
+class Cart {
+  static _addToCart(product = null, cart) {
+    let index = this._inCart(product, cart);
+    if (index != -1) {
+      cart.items[index].qty += 1;
+    } else {
+      let item = {
+        id: product.id,
+        imageURL: product.imageURL,
+        description: product.description,
+        price: product.price,
+        qty: product.qty
+      };
+      cart.items.push(item);
     }
+  }
 
-    static _removeFromCart(){
+  static _removeFromCart() {}
 
-    }
+  static _updateCart() {}
 
-    static _updateCart(){
+  static _inCart(item, cart) {
+    const id = element => element.id == item.id;
+    return cart.items.findIndex(id);
+  }
 
-    }
+  static _calculateTotal(cart) {
+    cart.items.reduce()
 
-    static _inCart(item, cart){
-        const id = element => element.id == item.id;
-        return cart.items.findIndex(id);
-    }
+  }
 
-    static _calculateTotal(){
+  static emptyCart() {}
 
-    }
-
-    static emptyCart() {
-
-    }
-
-    static setFormatTotals(){
-
-    }
-
-
+  static setFormatTotals() {}
 }
 
 module.exports = Cart;
